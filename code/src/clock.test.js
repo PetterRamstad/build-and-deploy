bda05e03ccea25e6d78186428d5fc88cf02181af
PetterRamstad <petterkramstad@gmail.com>
2024-11-12 19:45:22 +0100

    Fixed the test of right format

--- code/src/clock.test.js
+++ code/src/clock.test.js
@@ -1,10 +1,10 @@
-import { getSecondsLeftOfYear, getMinutesLeftOfYear } from "./clock";
+import { getSecondsLeftOfYear, getTimeString, getMinutesLeftOfYear } from "./clock";
 
-// test('getTimeString formats time string correctly', () => {
-//     const now = new Date(0, 0, 0, 10, 20, 30);
-//     const timeString = getTimeString(now);
-//     expect(timeString).toBe('10:20:30');
-// });
+test('getTimeString formats time string correctly', () => {
+    const now = new Date(0, 0, 0, 10, 20, 30);
+    const timeString = getTimeString(now);
+    expect(timeString).toBe('10.20.30');
+});
 
 test('getSecondsLeftOfYear returns the correct amount of seconds', () => {
     const now = new Date("2022-12-31T23:00:00.000Z");
